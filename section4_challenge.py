c8f080d12f8135ecf896c7be4aac4ba165724562
Praveen758092 <ch.praveenkumar462@gmail.com>
2020-09-26 17:22:39 +0530

    commit message

---
+++ section4_challenge.py
@@ -0,0 +1,19 @@
+print('Please choose any of the options below')
+
+choice='-'
+
+while True:
+    if choice=='0':
+        break
+    elif choice in '1234':
+        print('you have choosen {}'.format(choice))
+    else:
+        print('1.one')
+        print('2.two')
+        print('3.three')
+        print('4.four')
+
+    choice=input()
+
+
+
